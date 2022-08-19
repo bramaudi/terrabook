e3b6fcca86fb9762afee2f2aa863c51516f9dde4
Brama Udi <bramaudi.al@gmail.com>
2022-08-19 16:32:14 +0700

    Add browse wiki button

--- src/components/Wiki.tsx
+++ src/components/Wiki.tsx
@@ -1,5 +1,6 @@
 import { Base, Bosses, Hooks, NPC, Tools } from "@/types"
 import { createSignal, For, Show } from "solid-js"
+import { Browser } from "@capacitor/browser"
 import { parseImg } from "@/utils"
 import useJson from "@/hooks/useJson"
 import CraftTable from "@/components/CraftTable"
@@ -35,12 +36,18 @@ export default function(props: { slug: string }) {
 				<div class="flex items-center">
 					<div class="text-2xl font-bold mb-2">{items().title.replace('\\\'', "'")}</div>
 					<button
-						class="ml-auto"
+						class="bg-black bg-opacity-20 rounded-md p-2 mb-1 ml-auto"
+						onClick={async () => await Browser.open({ url: `https://terraria.wiki.gg/wiki/${name}` })}
+					>
+						<svg class="w-6" viewBox="0 0 24 24"><path fill="currentColor" d="M17.9 17.39c-.26-.8-1.01-1.39-1.9-1.39h-1v-3a1 1 0 0 0-1-1H8v-2h2a1 1 0 0 0 1-1V7h2a2 2 0 0 0 2-2v-.41a7.984 7.984 0 0 1 2.9 12.8M11 19.93c-3.95-.49-7-3.85-7-7.93c0-.62.08-1.22.21-1.79L9 15v1a2 2 0 0 0 2 2m1-16A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"></path></svg>
+					</button>
+					<button
+						class="bg-black bg-opacity-20 rounded-md p-2 mb-1 ml-2"
 						onClick={() => toggleFavorite(fav())}
 					>
 						<svg
 							classList={{ 'text-red-500': isFavorite(fav()) }}
-							class="w-5" viewBox="0 0 24 24"><path fill="currentColor" d="m10.65 19.8l-1.725-1.575q-2.65-2.425-4.787-4.813Q2 11.025 2 8.15Q2 5.8 3.575 4.225Q5.15 2.65 7.5 2.65q1.325 0 2.5.562q1.175.563 2 1.538q.825-.975 2-1.538q1.175-.562 2.5-.562q2.35 0 3.925 1.575Q22 5.8 22 8.15q0 2.875-2.125 5.275q-2.125 2.4-4.825 4.825l-1.7 1.55q-.575.525-1.35.525q-.775 0-1.35-.525Z"></path></svg>
+							class="w-6" viewBox="0 0 24 24"><path fill="currentColor" d="m10.65 19.8l-1.725-1.575q-2.65-2.425-4.787-4.813Q2 11.025 2 8.15Q2 5.8 3.575 4.225Q5.15 2.65 7.5 2.65q1.325 0 2.5.562q1.175.563 2 1.538q.825-.975 2-1.538q1.175-.562 2.5-.562q2.35 0 3.925 1.575Q22 5.8 22 8.15q0 2.875-2.125 5.275q-2.125 2.4-4.825 4.825l-1.7 1.55q-.575.525-1.35.525q-.775 0-1.35-.525Z"></path></svg>
 					</button>
 				</div>
 				<Show when={items().type === 'bosses'}>
@@ -79,14 +86,14 @@ export default function(props: { slug: string }) {
 						</caption>
 						<For each={Object.keys(items().statistics)}>
 							{key => (
-								<tr class="table-row px-2 border-b border-dashed border-white border-opacity-30">
+								<tr class="table-row px-2 border-b border-dashed border-white border-opacity-10">
 									<td class="font-semibold w-1/2 text-right pr-2">{key}</td>
 									<td class="w-1/2" innerHTML={parseImg(items().statistics[key])}></td>
 								</tr>
 							)}
 						</For>
 						<Show when={['pets','mounts'].includes(items().type)}>
-							<tr class="table-row px-2 border-b border-dashed border-white border-opacity-30">
+							<tr class="table-row px-2 border-b border-dashed border-white border-opacity-10">
 								<td class="align-baseline font-semibold w-1/2 text-right pr-2">Summon</td>
 								<td class="w-1/2">
 									<div>{slug}</div>
