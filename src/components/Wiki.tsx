d9bd141d1997192887477e0194700f33ec3270d7
Brama Udi <bramaudi.al@gmail.com>
2022-08-22 07:24:27 +0700

    Fix wiki button

--- src/components/Wiki.tsx
+++ src/components/Wiki.tsx
@@ -15,6 +15,12 @@ export default function(props: { slug: string }) {
 	// Exclusive on bosses display
 	const [mode, setMode] = createSignal(0) // [normal,expert,master]
 	
+	const wikiUrl = () => {
+		const slug = items()?.type === 'wings'
+			? `Wings#${props.slug.replace(/ /g, '_')}`
+			: props.slug
+		return `https://terraria.wiki.gg/wiki/${slug}`
+	}
 	const fav = () => {
 		const name = ['pets', 'mounts'].includes(items()?.type) ? items()?.title : props.slug
 		return { type: items().type, name }
@@ -38,7 +44,7 @@ export default function(props: { slug: string }) {
 					<div class="text-2xl font-bold mb-2">{items().title.replace('\\\'', "'")}</div>
 					<button
 						class="bg-black bg-opacity-20 rounded-md p-2 mb-1 ml-auto"
-						onClick={async () => await Browser.open({ url: `https://terraria.wiki.gg/wiki/${name}` })}
+						onClick={async () => await Browser.open({ url: wikiUrl() })}
 					>
 						<svg class="w-6" viewBox="0 0 24 24"><path fill="currentColor" d="M17.9 17.39c-.26-.8-1.01-1.39-1.9-1.39h-1v-3a1 1 0 0 0-1-1H8v-2h2a1 1 0 0 0 1-1V7h2a2 2 0 0 0 2-2v-.41a7.984 7.984 0 0 1 2.9 12.8M11 19.93c-3.95-.49-7-3.85-7-7.93c0-.62.08-1.22.21-1.79L9 15v1a2 2 0 0 0 2 2m1-16A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2Z"></path></svg>
 					</button>
