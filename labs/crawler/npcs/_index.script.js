21c5f36d8daa487d370026f25203aa116a337938
Brama Udi <bramaudi.al@gmail.com>
2022-09-30 20:57:17 +0700

    Update for Terraria v1.4.4 content

--- labs/crawler/npcs/_index.script.js
+++ labs/crawler/npcs/_index.script.js
@@ -3,7 +3,11 @@ const list = []
 for (const table of tables.splice(0, 4)) {
 	for (const tr of table.querySelectorAll('tr')) {
         const td = tr.querySelectorAll('td')
-        if (td.length > 1) list.push(td[1].textContent.trim())
+        if (td.length > 1) {
+            const internId = td[1].querySelector('.id')
+            internId?.remove()
+        	list.push(td[1].textContent.trim())
+        }
     }
 }
 print(list)
