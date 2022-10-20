4bf7e022461e0b11155b36bd552fcd2aeab14fa8
Brama Udi <bramaudi.al@gmail.com>
2022-10-20 19:22:42 +0700

    Fix wolf mount

--- labs/crawler/mounts/_index.script.js
+++ labs/crawler/mounts/_index.script.js
@@ -4,7 +4,7 @@ for (const table of tables) {
 	for (const tr of table.querySelectorAll('tr')) {
     	const td = tr.querySelectorAll('td')
 		if (td.length > 1) {
-            const nosuffix = ['Golf Cart','Flamingo','Witch\'s Broom','Rudolph']
+            const nosuffix = ['Golf Cart','Flamingo','Witch\'s Broom','Rudolph', 'Wolf']
             const name = td[0]
                 .querySelector('span span a')
                 .textContent.trim()
