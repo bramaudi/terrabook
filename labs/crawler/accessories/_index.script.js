33b02dcac8699eb4a4377e964c3e47437a7f674a
Brama Udi <bramaudi@pm.me>
2024-03-08 13:25:25 +0700

    fix accessories

--- labs/crawler/accessories/_index.script.js
+++ labs/crawler/accessories/_index.script.js
@@ -2,15 +2,11 @@ const table = document.querySelector('.terraria')
 	|| document.querySelector('.crafts .sortable')
 
 const list = []
-const only_golf = table.classList.contains('sortable')
-if (only_golf) {
-	table.querySelector('tr').remove()
-}
 
 for (const tr of table.querySelectorAll('tr')) {
     const td = tr.querySelectorAll('td')
 	if (td.length > 1) {
-        if (only_golf) removeTags(td[0], ['div'])
+        removeTags(td[0], ['span.id'])
     	list.push(
             td[0].textContent
                 .replace(/\(.*\)/g, '')
