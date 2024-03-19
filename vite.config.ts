b7a8488d2b1ae19835455052aa042eba46096fdd
Brama Udi <bramaudi@pm.me>
2024-03-19 13:38:09 +0700

    ignore some folder for faster dev server

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,10 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     watch: {
-      ignored: ['**/android/**']
+      ignored: [
+        '**/android/**',
+        '**/public/**',
+      ]
     }
   }
 });
